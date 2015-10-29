18f94d231e8ca694fdc2203e231a233ef0966e4b
uashraf90 <uashraf@outlook.com>
2015-10-29 10:30:10 -0400

    First commit
    First time commit

---
+++ ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Simpleapp
+//
+//  Created by Umer Ashraf on 10/14/15.
+//  Copyright © 2015 Umerashraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func buttoTapped(sender: AnyObject) {
+    }
+
+    @IBAction func new(sender: UITextField) {
+        
+        print("Hello press the button")
+        
+    }
+}
+
